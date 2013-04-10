e856c62befb354bd8193e0fc25d78593d7c42f83
Gordon Tillman <gtillman@mezeo.com>
2013-04-10 17:34:31 -0500

    Fix build errors with node version 0.10.3
    The process.js file that has been updated came from
    http://bugs.dojotoolkit.org/attachment/ticket/15620/process.js.
    
    This allows us to update the version of node.js running on bot
    so that we can also build the new GUI Installer.

--- build/node/process.js
+++ build/node/process.js
@@ -1,37 +1,39 @@
-define(["../fileHandleThrottle", "../messages"], function(fht, messages) {
-	var spawn= require.nodeRequire("child_process").spawn;
+define(["../fileHandleThrottle", "../messages"], function(fht, messages){
+	var spawn = require.nodeRequire("child_process").spawn;
 	return {
 		cwd:process.cwd,
 		exit:function(code){
 			// no more messages
 			messages.stop();
 
-			// allow whatever is in the stdout buffer to be pumped
-			process.stdout.on('close', function(){
-				process.exit(code);
-			});
+			process.exit(code);
 		},
 
 		exec:function() {
 			// signature is (command, arg1, ..., argn, errorMessage, bc, callback)
-			for(var command= arguments[0], args= [], i= 1; i<arguments.length-3; i++){
+			for(var command = arguments[0], args = [], i = 1; i<arguments.length-3; i++){
 				args.push(arguments[i]);
 			}
 			var
-				errorMessage= arguments[i++],
-				bc= arguments[i++],
-				callback= arguments[i];
+				errorMessage = arguments[i++],
+				bc = arguments[i++],
+				callback = arguments[i];
 			fht.enqueue(function(){
 				var
-					text= "",
-					process= spawn(command, args);
-				process.on("exit", function(code){
-					fht.release();
-					if(code){
-						bc.log("execFailed", ["message", errorMessage, "output", text]);
-					}
-					callback && callback(code, text);
-				});
+					text = "",
+					process = spawn(command, args),
+					status = 0,
+					finish = function(code){
+						if(++status===2){
+							fht.release();
+							if(code){
+								bc.log("execFailed", ["message", errorMessage, "output", text]);
+							}
+							callback && callback(code, text);
+						}
+					};
+				process.on("exit", finish);
+				process.on("close", finish);
 				process.stdout.on("data", function(data){
 					text+= data;
 				});
